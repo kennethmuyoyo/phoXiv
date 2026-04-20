75074b9359dc7fef1ad1d32fca5be2f93e96b6ca
Kenneth Muyoyo <kennethmuyoyo@gmail.com>
2026-04-20 10:56:55 +0800

    Initial Commit

---
+++ phoXiv/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  phoXiv
+//
+//  Created by Kenneth Muyoyo on 20/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ phoXiv/phoXivApp.swift
@@ -0,0 +1,17 @@
+//
+//  phoXivApp.swift
+//  phoXiv
+//
+//  Created by Kenneth Muyoyo on 20/04/26.
+//
+
+import SwiftUI
+
+@main
+struct phoXivApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
